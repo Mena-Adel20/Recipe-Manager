bef4d972e5855f4adf80fdad649bcfb23f3a176f
Mena-Adel20 <menaadel1276@gmail.com>
2024-10-11 18:05:55 +0200

    create page for categories

---
+++ static/js/categories.js
@@ -0,0 +1,10 @@
+const categoryItem = document.querySelectorAll(".category-card");
+
+categoryItem.forEach(function(item) {
+    item.addEventListener("click", function() {
+        // Get the data-id attribute value from the clicked item
+        const categoryName = item.dataset.id; 
+        // Redirect the user to the recipe details page .
+        window.location.href = "/category/" + categoryName;
+    });
+});
