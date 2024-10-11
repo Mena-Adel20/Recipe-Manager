4780e52570da3472e1ce670155575cf667ed1e09
Mena-Adel20 <menaadel1276@gmail.com>
2024-10-11 17:46:06 +0200

    home page
    put card for categories and make style for this page
    and js to card to click on it and take me to categories page

---
+++ static/js/home.js
@@ -0,0 +1,8 @@
+const categoryCard = document.getElementById("category-card");
+
+
+// Redirect to category page when clicking on the element with id "categoryCard"
+categoryCard.addEventListener("click", function() {
+    window.location.href = "/category";
+});
+
