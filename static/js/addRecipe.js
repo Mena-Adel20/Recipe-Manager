86d017362c5acb5a3155289ba9e43dbfee39b042
Mena-Adel20 <menaadel1276@gmail.com>
2024-10-12 17:32:26 +0200

    create form for add new recipe

---
+++ static/js/addRecipe.js
@@ -0,0 +1,15 @@
+ function addIngredient() {
+            const ingredientsDiv = document.getElementById('ingredients');
+            const newIngredientDiv = document.createElement('div');
+            newIngredientDiv.classList.add('ingredient');
+            newIngredientDiv.innerHTML = `
+                <input type="text" name="ingredientName[]" placeholder="Ingredient Name" required>
+                <input type="text" name="ingredientMeasurement[]" placeholder="Measurement" required>
+                <button type="button" onclick="removeIngredient(this)">Remove</button>
+            `;
+            ingredientsDiv.appendChild(newIngredientDiv);
+        }
+
+        function removeIngredient(button) {
+            button.parentNode.remove();
+        }
