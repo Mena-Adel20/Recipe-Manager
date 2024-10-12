b2e03457d06023f409f90d29d0317f4410a3317e
Mena-Adel20 <menaadel1276@gmail.com>
2024-10-12 10:24:38 +0200

    Update categories.js

--- static/js/categories.js
+++ static/js/categories.js
@@ -8,3 +8,15 @@ categoryItem.forEach(function(item) {
         window.location.href = "/category/" + categoryName;
     });
 });
+
+
+const recipeItems = document.querySelectorAll(".recipe-list-card");
+
+recipeItems.forEach(function(item) {
+    item.addEventListener("click", function() {
+        // Get the data-id attribute value from the clicked item
+        const recipeId = item.dataset.id; 
+        // Redirect the user to the recipe details page 
+        window.location.href = `/recipe-details/${recipeId}`; 
+    });
+});
