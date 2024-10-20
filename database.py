162780e67d0b48c848b9a42418b274ce673f37a2
Mena-Adel20 <menaadel1276@gmail.com>
2024-10-20 15:38:28 +0200

    all function that used to read or save data

---
+++ database.py
@@ -0,0 +1,14 @@
+import json
+
+class Database:
+    def __init__(self, recipe_file='recipes.json', user_file='users.json'):
+        self.recipe_file = recipe_file
+        self.user_file = user_file
+
+    def load_json(self, filename):
+        with open(filename, 'r+') as f:
+            return json.load(f)
+
+    def save_json(self, filename, data):
+        with open(filename, 'w') as f:
+            json.dump(data, f, indent=4)
