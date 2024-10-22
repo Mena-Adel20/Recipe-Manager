ff347737c87407964472be86423d44ab4ad72274
Mena-Adel20 <menaadel1276@gmail.com>
2024-10-22 17:15:37 +0200

    write some logic here for functions of user

--- user_manager.py
+++ user_manager.py
@@ -4,6 +4,7 @@ import json
 class UserManager:
     def __init__(self, db: Database):
         self.db = db
+#************************************************************************************
 
     def add_user(self, email, password, username):
         try:
@@ -25,26 +26,46 @@ class UserManager:
         self.db.save_json(self.db.user_file, data)
         return None
 
+#************************************************************************************
+
+    # save recipe id to user
     def add_recipe_to_user(self, email, recipe_id):
         data = self.db.load_json(self.db.user_file)
+        #find the user that add recipe and add id of recipe in his list of recipes 
         for user in data['users']:
             if user['email'] == email:
                 user['recipes'].append(str(recipe_id))
                 break
         self.db.save_json(self.db.user_file, data)
+#************************************************************************************
 
     def get_user_recipes(self, email):
         user_data = self.db.load_json(self.db.user_file)
         recipe_data = self.db.load_json(self.db.recipe_file)
-        
-        user_recipe_ids = next((user['recipes'] for user in user_data['users'] 
-                              if user['email'] == email), [])
-        return [r for r in recipe_data['recipes'] if r['id'] in user_recipe_ids]
+        # first get user form data and then get the list of his recipes  
+        user_recipe_ids = []
+        for user in user_data['users']:
+            if user['email'] == email:
+                user_recipe_ids = user['recipes']
+                break  
+        # Now filter the recipes based on the user_recipe_ids
+        filtered_recipes = []
+        for r in recipe_data['recipes']:
+            if r['id'] in user_recipe_ids:
+                filtered_recipes.append(r)
 
+        return filtered_recipes
+#************************************************************************************
+
+#this function to remove a specific recipe from a user's list of recipes in a JSON data structure that stores user information. 
     def remove_recipe_from_user(self, email, recipe_id):
         data = self.db.load_json(self.db.user_file)
         for user in data['users']:
             if user['email'] == email:
-                user['recipes'] = [r for r in user['recipes'] if r != recipe_id]
+                updated_recipes = []
+                for r in user['recipes']:
+                    if r != recipe_id:  
+                        updated_recipes.append(r)  
+                user['recipes'] = updated_recipes  
                 break
         self.db.save_json(self.db.user_file, data)
